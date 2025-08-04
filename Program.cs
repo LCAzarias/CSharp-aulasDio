774df4209fc6cc0477f852aeb6481cc5b779f745
Luis <akade.luis@gmail.com>
2025-08-03 22:29:03 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DIO___C_.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pessoa p = new Pessoa()
+        {
+            Nome = "Luis",
+            Idade = 61,
+        };
+        p.Apresentar();
+
+        p.
+    }
+}
+

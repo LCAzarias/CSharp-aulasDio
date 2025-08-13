07f8523da8dc1161ebc8b19b400f91bc8cad3060
Luis <akade.luis@gmail.com>
2025-08-13 07:19:22 -0300

    Atualização aula C#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using DIO.Models;
+
+int quantidadeEstoque = 10;
+int quantidadeVenda = 4;
+bool possivelVenda = quantidadeEstoque >= quantidadeVenda;
+
+Console.WriteLine($"Quantidade em Estoque:{quantidadeEstoque}");
+Console.WriteLine($"Quantidade Venda:{quantidadeVenda}");
+Console.WriteLine($"É possivel realizar a venda:{possivelVenda}");
+
+
+if (possivelVenda)
+{
+    Console.WriteLine("Venda Autorizada");
+}
+else
+{
+    Console.WriteLine("Venda não Autorizada");
+}

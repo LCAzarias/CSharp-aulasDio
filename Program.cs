1f7f37ce40fd68e856e6cb276f2847c3a6b575ca
Luis <akade.luis@gmail.com>
2025-09-02 06:21:03 -0300

    modified class

--- Program.cs
+++ Program.cs
@@ -1,20 +1,154 @@
 ﻿
+
+
 using DIO.Models;
 
-int quantidadeEstoque = 10;
-int quantidadeVenda = 4;
-bool possivelVenda = quantidadeEstoque >= quantidadeVenda;
+Calculadora calc = new Calculadora();
 
-Console.WriteLine($"Quantidade em Estoque:{quantidadeEstoque}");
-Console.WriteLine($"Quantidade Venda:{quantidadeVenda}");
-Console.WriteLine($"É possivel realizar a venda:{possivelVenda}");
+calc.Somar(10, 30);
+calc.Subtrair(25, 10);
+calc.Multiplicar(24, 6);
+calc.Dividir(30, 10);
+calc.Potencia(6, 2);
 
 
-if (possivelVenda)
-{
-    Console.WriteLine("Venda Autorizada");
-}
-else
-{
-    Console.WriteLine("Venda não Autorizada");
-}
+
+
+
+
+
+
+// int quantidadeEstoque;
+// Console.WriteLine("Digite Quantidade de Estoque:");
+// while(!int.TryParse(Console.ReadLine(), out quantidadeEstoque));
+// int quantidadeVenda = 8 ;
+// bool possivelVenda = quantidadeVenda > 0 && quantidadeEstoque >= quantidadeVenda;
+
+// Console.WriteLine($"Quantidade em Estoque:{quantidadeEstoque}");
+// Console.WriteLine($"Quantidade Venda:{quantidadeVenda}");
+// Console.WriteLine($"É possivel realizar a venda:{possivelVenda}");
+
+// if (quantidadeVenda == 0)
+// {
+//     Console.WriteLine("Venda Inválida!");
+// }
+// else if (possivelVenda) 
+// {
+//     Console.WriteLine("Venda Autorizada");
+// }
+// else
+// {
+//     Console.WriteLine("Venda não Autorizada");
+// }
+
+//using DIO.Models;
+
+// int quantidadeEstoque = 10;
+// int quantidadeVenda = 8 ;
+// bool possivelVenda = quantidadeVenda > 0 && quantidadeEstoque >= quantidadeVenda;
+
+// Console.WriteLine($"Quantidade em Estoque:{quantidadeEstoque}");
+// Console.WriteLine($"Quantidade Venda:{quantidadeVenda}");
+// Console.WriteLine($"É possivel realizar a venda:{possivelVenda}");
+
+// if (quantidadeVenda == 0)
+// {
+//     Console.WriteLine("Venda Inválida!");
+// }
+// else if (possivelVenda) 
+// {
+//     Console.WriteLine("Venda Autorizada");
+// }
+// else
+// {
+//     Console.WriteLine("Venda não Autorizada");
+// }
+
+//using DIO.Models;
+
+// int quantidadeEstoque = 10;
+// int quantidadeVenda = 8 ;
+// bool possivelVenda = quantidadeVenda > 0 && quantidadeEstoque >= quantidadeVenda;
+
+// Console.WriteLine($"Quantidade em Estoque:{quantidadeEstoque}");
+// Console.WriteLine($"Quantidade Venda:{quantidadeVenda}");
+// Console.WriteLine($"É possivel realizar a venda:{possivelVenda}");
+
+// if (quantidadeVenda == 0)
+// {
+//     Console.WriteLine("Venda Inválida!");
+// }
+// else if (possivelVenda) 
+// {
+//     Console.WriteLine("Venda Autorizada");
+// }
+// else
+// {
+//     Console.WriteLine("Venda não Autorizada");
+// }
+
+//using DIO.Models;
+
+// int quantidadeEstoque = 10;
+// int quantidadeVenda = 8 ;
+// bool possivelVenda = quantidadeVenda > 0 && quantidadeEstoque >= quantidadeVenda;
+
+// Console.WriteLine($"Quantidade em Estoque:{quantidadeEstoque}");
+// Console.WriteLine($"Quantidade Venda:{quantidadeVenda}");
+// Console.WriteLine($"É possivel realizar a venda:{possivelVenda}");
+
+// if (quantidadeVenda == 0)
+// {
+//     Console.WriteLine("Venda Inválida!");
+// }
+// else if (possivelVenda) 
+// {
+//     Console.WriteLine("Venda Autorizada");
+// }
+// else
+// {
+//     Console.WriteLine("Venda não Autorizada");
+// }
+
+//using DIO.Models;
+
+// int quantidadeEstoque = 10;
+// int quantidadeVenda = 8 ;
+// bool possivelVenda = quantidadeVenda > 0 && quantidadeEstoque >= quantidadeVenda;
+
+// Console.WriteLine($"Quantidade em Estoque:{quantidadeEstoque}");
+// Console.WriteLine($"Quantidade Venda:{quantidadeVenda}");
+// Console.WriteLine($"É possivel realizar a venda:{possivelVenda}");
+
+// if (quantidadeVenda == 0)
+// {
+//     Console.WriteLine("Venda Inválida!");
+// }
+// else if (possivelVenda) 
+// {
+//     Console.WriteLine("Venda Autorizada");
+// }
+// else
+// {
+//     Console.WriteLine("Venda não Autorizada");
+// }
+
+// Console.WriteLine("Digite uma Vogal:");
+// string letra = Console.ReadLine();
+
+// switch (letra)
+// {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+
+//         Console.WriteLine("Vogal");
+//         break;
+
+//     default:
+//         Console.WriteLine("Não é uma Vogal!");
+//         break;
+
+// }
